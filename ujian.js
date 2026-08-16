aa7c87ef631338a4e55d6d94ea51a69f19a9b08d
Agus Tjakra <49585004+gsdiredja@users.noreply.github.com>
2026-08-16 08:47:30 +0700

    Update ujian.js

--- ujian.js
+++ ujian.js
@@ -2,7 +2,7 @@ let currentQuestionIndex = 0;
 let questionsData = [];
 let userAnswers = {};
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbySm7Bw7wgYVZwu7cs6xAuXyQc1Y-FUxJuiJ80YxyFOQeLSNfsrBQ0dgsYN4B_9LZgg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxNtxm-31dvUr2ebTKFwrkr_Avv3vwHKtzA6djrt297RSlLxI0eW2COPWnMTE3IJjwl/exec";
 
 let EXAM_DURATION_MINUTES = 60;
 let totalSeconds = EXAM_DURATION_MINUTES * 60;
